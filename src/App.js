94a521155b87a1ac39c90d499be10e1bf15e925f
Louisa Davtyan <luidavtyan@gmail.com>
2019-02-07 17:44:46 +0400

    some changes

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
 
     componentDidMount() {
         this.authListener();
-        askForPermissioToReceiveNotifications();
+     askForPermissioToReceiveNotifications();
     }
 
     authListener() {
